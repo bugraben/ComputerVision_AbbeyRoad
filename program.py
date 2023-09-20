da9a21251031ddac32358ecedae35527bcc68523
Bugra Isikdemir <28690548+bugraben@users.noreply.github.com>
2023-09-20 03:52:42 +0300

    It works.

--- program.py
+++ program.py
@@ -14,12 +14,13 @@ in_height = int(in_width / ratio[0] * ratio[1])
 samplingRate = 2
 out_width = 960
 out_height= int(out_width / ratio[0] * ratio[1])
-sample_count = 10
+sample_count = 12
 
 frames = np.array([], dtype="uint8")
 
 def processChannel(channel = None, frames = None, result = None):
     oneChannelFrame = np.zeros((in_height, in_width), dtype="uint8")
+    oneChannelFrame_visualize = np.zeros((in_height, in_width, 3), dtype="uint8")
     if channel in BGR_CHANNELS:
         for row in range(0,in_height):
             print(f"ROW: {row} | CHA: {channel_names[channel]}")
@@ -29,16 +30,17 @@ def processChannel(channel = None, frames = None, result = None):
                     pixel = np.append(pixel,frames[fr,row,column,channel])
                     
                 oneChannelFrame[row, column] = pixel.mean()
+                oneChannelFrame_visualize[:, :, channel] = oneChannelFrame
+            cv.imshow(channel_names[channel], oneChannelFrame_visualize)
+            cv.waitKey(1)
+        cv.destroyAllWindows()
         result.append(oneChannelFrame)
                 
     else:
         raise ValueError("results: Argument 'channel' must be one of %r." % BGR_CHANNELS)
 
-# cv.imshow(fileName, outputFrame)
-# cv.waitKey(500)
-# def monitorOutput():
-#         cv.imshow(fileName, outputFrame)
-#         cv.waitKey(1)
+
+
 
 
 if __name__ == '__main__':
@@ -61,7 +63,7 @@ if __name__ == '__main__':
     cv.destroyAllWindows()
     print("Closed")
 
-    frames = frames.reshape(sample_count,in_height,in_width,3)
+    frames = frames.reshape(sample_count, in_height, in_width, 3)
     
     manager = Manager()
 
@@ -73,7 +75,6 @@ if __name__ == '__main__':
     p_blue = Process(target=processChannel, args=(0, frames, channel_blue))
     p_green = Process(target=processChannel, args=(1, frames, channel_green))
     p_red = Process(target=processChannel, args=(2, frames, channel_red))
-    # p_monitor = Process(target=monitorOutput)
 
 
     p_blue.start()
@@ -85,33 +86,11 @@ if __name__ == '__main__':
     p_red.join()
     print("Processes OK.")
 
+    finalFrame = np.zeros((in_height, in_width, 3), dtype = "uint8")
 
-    finalFrame = np.array([channel_blue[0], channel_green[0], channel_red[0]], dtype="uint8")
-
-    finalFrame = finalFrame.reshape(in_height,in_width)
-
-    print(finalFrame)
-
-    # cv.imshow(fileName, channel_blue)
-    # cv.waitKey(1)
-    # cv.imshow(fileName, channel_green)
-    # cv.waitKey(1)
-    # cv.imshow(fileName, channel_red)
-    # cv.waitKey(1)
-    # sleep(5000)
-
-    # while p_blue.is_alive() or p_green.is_alive() or p_red.is_alive():
-    #     p_monitor.start()
-    #     p_monitor.join()
-    # else:
-    #     cv.destroyAllWindows()
-
-
-    # cv.imwrite(".png",channel_blue)
-    # cv.imwrite("GREEN.png",channel_green)
-    # cv.imwrite("RED.png",channel_red)
-
-
+    finalFrame[:, :, 0] = channel_blue[0]
+    finalFrame[:, :, 1] = channel_green[0]
+    finalFrame[:, :, 2] = channel_red[0]
 
     outputRsz = cv.resize(finalFrame, (out_width,out_height))
     cv.imwrite(f"output_{fileName.split('.')[0]}_{sample_count}_samples.png",outputRsz)
