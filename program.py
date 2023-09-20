224a53a20cd5fc756cf19d0b815d1c8500a7b25a
Bugra Isikdemir <28690548+bugraben@users.noreply.github.com>
2023-09-20 03:07:12 +0300

    Works for one channel.

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 from time import sleep
 import numpy as np
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
 BGR_CHANNELS = (0, 1, 2)
 channel_names = ["BLUE", "GREEN", "RED"]
@@ -17,9 +17,9 @@ out_height= int(out_width / ratio[0] * ratio[1])
 sample_count = 10
 
 frames = np.array([], dtype="uint8")
-outputFrame = np.zeros((in_height,in_width,2), dtype="uint8")
+outputFrame = np.zeros((in_height, in_width), dtype="uint8")
 
-def processChannel(channel = None, frames = None):
+def processChannel(channel = None, frames = None, result = None):
     print(f"Process {channel} has started.")
     if channel in BGR_CHANNELS:
         for row in range(0,in_height):
@@ -30,8 +30,8 @@ def processChannel(channel = None, frames = None):
                     pixel = np.append(pixel,frames[fr,row,column,channel])
                     
                 global outputFrame
-                outputFrame[row,column] = pixel.mean()
-        return outputFrame
+                outputFrame[row, column] = pixel.mean()
+        result.append(outputFrame)
                 
     else:
         raise ValueError("results: Argument 'channel' must be one of %r." % BGR_CHANNELS)
@@ -64,18 +64,20 @@ if __name__ == '__main__':
     print("Closed")
 
     frames = frames.reshape(sample_count,in_height,in_width,3)
+    
+    manager = Manager()
+
+    channel_blue = manager.list()
 
 
-
-
-    p_blue = Process(target=processChannel, args=(0, frames,))
+    p_blue = Process(target=processChannel, args=(0, frames, channel_blue))
     # p_green = Process(target=processChannel, args=(1, frames,))
     # p_red = Process(target=processChannel, args=(2, frames,))
     # p_monitor = Process(target=monitorOutput)
 
     # p_blue.start()
 
-    channel_blue = p_blue.start()
+    p_blue.start()
     # channel_green = p_green.start()
     # channel_red = p_red.start()
 
@@ -83,6 +85,12 @@ if __name__ == '__main__':
     print("Process OK.")
     # p_green.join()
     # p_red.join()
+
+
+    channel_blue = np.array(channel_blue, dtype="uint8")
+    channel_blue = channel_blue.reshape(in_height,in_width)
+
+    print(type(np.array(channel_blue)))
 
     # cv.imshow(fileName, channel_blue)
     # cv.waitKey(1)
@@ -100,7 +108,7 @@ if __name__ == '__main__':
 
     print(channel_blue)
 
-    # cv.imwrite("BLUE.png",channel_blue)
+    cv.imwrite("BLUE.png",channel_blue)
     # cv.imwrite("GREEN.png",channel_green)
     # cv.imwrite("RED.png",channel_red)
 
