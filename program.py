973d3eb2e6f62badc107e0b88ff05f367022cf75
Bugra Isikdemir <28690548+bugraben@users.noreply.github.com>
2023-09-28 20:38:50 +0300

    Trying reduce speed by splitting numpy arrays.

--- program.py
+++ program.py
@@ -2,21 +2,24 @@ import cv2 as cv
 from time import sleep
 import numpy as np
 from multiprocessing import Process, Manager
+from datetime import datetime
 
 BGR_CHANNELS = (0, 1, 2)
 channel_names = ["BLUE", "GREEN", "RED"]
 
-
-fileName = "Abbey_Road.mov"
+date = datetime.now()
+fileName = "speed_up.mp4"
 ratio = [16,9]
-in_width = 960   
+in_width = 1920   
 in_height = int(in_width / ratio[0] * ratio[1])
 samplingRate = 1
-out_width = 1280
+out_width = 1920
 out_height= int(out_width / ratio[0] * ratio[1])
-sample_count = 475
+sample_count = 160
+preview_resolution = (int(in_width*0.35) , int(in_height*0.35))
 
-frames = np.array([], dtype="uint8")
+
+outputImage = np.zeros((in_height, in_width, 3), dtype = "uint8")
 
 def processChannel(channel = None, frames = None, result = None):
     oneChannelFrame = np.zeros((in_height, in_width), dtype="uint8")
@@ -30,7 +33,7 @@ def processChannel(channel = None, frames = None, result = None):
                     pixel = np.append(pixel,frames[fr,row,column,channel])
                     
                 oneChannelFrame[row, column] = pixel.mean()
-            oneChannelFrame_visualize[:, :, channel] = cv.putText(oneChannelFrame, f"{row}/{in_height}", (50, 50), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (50), 4)
+            oneChannelFrame_visualize[:, :, channel] = cv.putText(cv.resize(oneChannelFrame, preview_resolution), f"{row}/{in_height}", (50, 50), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (50), 4)
             cv.imshow(channel_names[channel], oneChannelFrame_visualize)
             cv.waitKey(1)
         cv.destroyAllWindows()
@@ -39,33 +42,52 @@ def processChannel(channel = None, frames = None, result = None):
     else:
         raise ValueError("results: Argument 'channel' must be one of %r." % BGR_CHANNELS)
 
-
-
-
-if __name__ == '__main__':
+def captureSequence():
+    frames_0 = np.array([], dtype="uint8")
+    frames_1 = np.array([], dtype="uint8")
+    frames_2 = np.array([], dtype="uint8")
+    frames_3 = np.array([], dtype="uint8")
     cap = cv.VideoCapture(fileName)
-
-    for a in range(0,sample_count):
+    for a in range(sample_count):
         ret, frame = cap.read()
         if ret == True:
             # sleep(0.001)
             frame = cv.resize(frame,(in_width,in_height))
-            frames = np.append(frames, frame)
-            cv.imshow("Video", cv.putText(frame, str(a), (50, 50), cv.FONT_HERSHEY_PLAIN, 3, (255 , 255, 255), 3))
+            # frames = np.append(frames, frame)
+
+            if a < 400:
+                frames_0 = np.append(frames_0, frame)
+            elif a < 800:
+                frames_2 = np.append(frames_1, frame)
+            elif a < 1200:
+                frames_2 = np.append(frames_2, frame)
+            else:
+                frames_3 = np.append(frames_3, frame)
+
+            # print(a)
+            cv.imshow("Video", cv.resize(cv.putText(frame, str(a), (50, 50), cv.FONT_HERSHEY_PLAIN, 3, (255 , 255, 255), 3) , preview_resolution))
 
         else: 
-            print("No more frames.")
+            print("Not enough samples!")
             break
 
         if cv.waitKey(1) & 0xFF == ord("q"):
             break
 
+
     cap.release()
     cv.destroyAllWindows()
     print("Closed")
 
+    frames = np.array([frames_0, frames_1, frames_2, frames_3], dtype="uint8")
     frames = frames.reshape(sample_count, in_height, in_width, 3)
-    
+    return frames
+
+if __name__ == '__main__':
+
+    captureSequence()
+
+
     manager = Manager()
 
     channel_blue = manager.list()
@@ -81,18 +103,19 @@ if __name__ == '__main__':
     p_blue.start()
     p_green.start()
     p_red.start()
+    print("Processes have been started.")
+
 
     p_blue.join()
     p_green.join()
     p_red.join()
     print("Processes OK.")
 
-    finalFrame = np.zeros((in_height, in_width, 3), dtype = "uint8")
 
-    finalFrame[:, :, 0] = channel_blue[0]
-    finalFrame[:, :, 1] = channel_green[0]
-    finalFrame[:, :, 2] = channel_red[0]
+    outputImage[:, :, 0] = channel_blue[0]
+    outputImage[:, :, 1] = channel_green[0]
+    outputImage[:, :, 2] = channel_red[0]
 
-    outputRsz = cv.resize(finalFrame, (out_width,out_height))
-    cv.imwrite(f"output_{fileName.split('.')[0]}_{sample_count}_samples.png",outputRsz)
+    outputRsz = cv.resize(outputImage, (out_width,out_height))
+    cv.imwrite(f"output_{fileName.split('.')[0]}_{sample_count}_samples_{date.day}{date.month}{date.year}.png",outputRsz)
 
